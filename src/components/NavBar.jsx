56faf041549fba247dcd4e07b367aa0a65493c92
David Urbano <david.urbano_code@outlook.com>
2022-12-08 04:05:20 -0500

    Solo falta version desktop del navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,16 +4,19 @@ import { useState } from 'react';
 const NavBar = () => {
 	const [burger_class, setBurgerClass] = useState('burger-bar unclicked');
 	const [menu_class, setMenuClass] = useState('menu hidden');
+	const [luclass, setLuclass] = useState('noshow');
 	const [isMenuClicked, setIsMenuClicked] = useState(false);
 
 	// toggle burger menu change
 	const updateMenu = () => {
 		if (!isMenuClicked) {
 			setBurgerClass('burger-bar clicked');
+			setLuclass('show');
 			setMenuClass('menu visible');
 		} else {
 			setBurgerClass('burger-bar unclicked');
 			setMenuClass('menu hidden');
+			setLuclass('noshow');
 		}
 		setIsMenuClicked(!isMenuClicked);
 	};
@@ -29,21 +32,27 @@ const NavBar = () => {
 			</nav>
 
 			<div className={menu_class}>
-				<ul>
+				<ul className={luclass}>
 					<li>
+						<p>01.</p>
 						<a>About</a>
 					</li>
 					<li>
+						<p>02.</p>
 						<a>Experience</a>
 					</li>
 					<li>
+						<p>03.</p>
 						<a>Work</a>
 					</li>
 					<li>
+						<p>04.</p>
 						<a>Contact</a>
 					</li>
 					<li>
-						<a></a>
+						<span>
+							<a className='resume-link'>Resume</a>
+						</span>
 					</li>
 				</ul>
 			</div>
