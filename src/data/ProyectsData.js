caf8369a396fe85fefe0f4613c4a6a852dd4249c
David-UM <david.urbano_code@outlook.com>
2023-01-17 20:48:46 -0500

    Nuevo example of data

---
+++ src/data/ProyectsData.js
@@ -0,0 +1,114 @@
+const data = [
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: true,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: true,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: true,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: false,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: false,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: false,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: false,
+	},
+	{
+		Category: 'Basic Project',
+		Title: 'My Gallery',
+		Description:
+			'a web page where I save photos that I like and that has a search bar to be able to find certain photos more easily',
+		DeepDescription:
+			'This is a web page where I show the images that I like, it contains a search bar, a text that indicates the number of results and is responsive',
+		Technologies: ['React', 'Vite', 'HTML', 'CSS'],
+		ImageLink:
+			'https://raw.githubusercontent.com/David-UM/Portfolio/main/src/assets/MyGallery.jpg',
+		RepositoryLink: 'https://github.com/David-UM/L-Gallery',
+		Key: 101,
+		Especial: false,
+	},
+];
